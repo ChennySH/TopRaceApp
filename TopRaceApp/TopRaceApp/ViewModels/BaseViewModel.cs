93bf11b21973cf53b46b422271761ab5f95ea1ba
chenny shahir <chenny.shahir@gmail.com>
2021-12-11 17:51:41 +0200

    Add project files.

---
+++ TopRaceApp/TopRaceApp/ViewModels/BaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace DealAndTripApp.ViewModels
+{
+    class BaseViewModel : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
