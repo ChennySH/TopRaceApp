5a027df1d4d251e8e166aaaa3039e5e9e499fc3a
chenny shahir <chenny.shahir@gmail.com>
2021-12-20 17:53:48 +0200

    sync

--- TopRaceApp/TopRaceApp/Models/User.cs
+++ TopRaceApp/TopRaceApp/Models/User.cs
@@ -11,7 +11,7 @@ namespace TopRaceApp.Models
         public string Email { get; set; }
         public string Password { get; set; }
         public string PhoneNumber { get; set; }
-        public int PlayerId { get; set; }
+        public int? PlayerId { get; set; }
 
         public virtual Player Player { get; set; }
     }
