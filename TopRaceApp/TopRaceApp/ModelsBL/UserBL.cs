9ec05662e80f272cfa7acc4e3cd1c2518afbc573
User <User@LAB3-8>
2022-03-28 14:45:10 +0300

    asyxn

---
+++ TopRaceApp/TopRaceApp/ModelsBL/UserBL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TopRaceApp.Models
+{
+    public partial class UserBL
+    {
+        public string Color
+        {
+            get; set;
+        }
+
+    }
+}
